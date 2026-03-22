add3c25bb22b11dbdbb10d7b07ea3d840ced1035
miswah <miswah.md@gmail.com>
2026-03-22 15:13:27 +0530

    (feat): grpc service mock createBillingAccount method

--- billing-service/src/main/java/com/miswah/billing_service/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/miswah/billing_service/grpc/BillingGrpcService.java
@@ -1,4 +1,25 @@
 package com.miswah.billing_service.grpc;
 
-public class BillingGrpcService {
+import billing.BillingResponse;
+import billing.BillingServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
+
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+
+    @Override
+    public void createBillingAccount(billing.BillingRequest billingRequest, StreamObserver<BillingResponse> responseObserver){
+        log.info("Create Billing Account request received {}", billingRequest.toString());
+        //logic
+
+        BillingResponse response = BillingResponse.newBuilder().setAccountId("1234").setStatus("ACTIVE").build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
 }
