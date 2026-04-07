4e4f29a6b937ecc288119e2a869998f23eb0216b
miswah <miswah.md@gmail.com>
2026-04-07 23:47:07 +0530

    (enh): kafka consumer for analyticservice

---
+++ analytics-service/src/test/java/com/miswah/analytics_service/analytics_service/AnalyticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miswah.analytics_service.analytics_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
