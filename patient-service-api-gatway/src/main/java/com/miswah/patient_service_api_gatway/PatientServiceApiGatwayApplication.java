db73d68d9b0a7ecfdf62216da2f6b44e9d1886e7
miswah <miswah.md@gmail.com>
2026-04-28 22:39:49 +0530

    (feat): docker compose with property changes

---
+++ patient-service-api-gatway/src/main/java/com/miswah/patient_service_api_gatway/PatientServiceApiGatwayApplication.java
@@ -0,0 +1,13 @@
+package com.miswah.patient_service_api_gatway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientServiceApiGatwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientServiceApiGatwayApplication.class, args);
+	}
+
+}
---
+++ patient-service-api-gatway/src/test/java/com/miswah/patient_service_api_gatway/PatientServiceApiGatwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miswah.patient_service_api_gatway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApiGatwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
